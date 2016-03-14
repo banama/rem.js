52148c8b06758749755fa289276803a682422164
banama <306672962@qq.com>
2016-03-14 09:45:03 +0800

    add rem2px and px2rem

--- rem.js
+++ rem.js
@@ -1,12 +1,12 @@
 ;(function(){
-    var agent = window.navigator.userAgent.toLowerCase();
-    var doc = window.document;
-    var dpr = window.devicePixelRatio;
-    var width = doc.documentElement.getBoundingClientRect().width;
-    var dpr = /iphone/g.test(agent) || /ipad/g.test(agent) ? "dpr"+dpr : "dpr1";
-    var meta = doc.createElement("meta");
+    var width = document.documentElement.getBoundingClientRect().width;
+    document.documentElement.style.fontSize = document.documentElement.getBoundingClientRect().width/10+"px"
+    var rem = {}
+    rem.toPx = function(rem){
+        return rem * width / 10
+    }
+    rem.toRem = function(px){
+        return px * 10 / width
+    }
+})()
 
-    doc.documentElement.classList.add(dpr)
-    doc.documentElement.classList.add(/android/g.test(agent) ? "and" : "ios")
-    doc.documentElement.style.fontSize = width/10 + "px";
-})(window)
